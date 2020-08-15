a6f3a11a19a8f64075ed91a4684ae17994c63831
anand iyengar <anandiyengar0@gmail.com>
2020-08-15 23:18:10 +0530

    FIRST COMMIT-5

--- app.js
+++ app.js
@@ -44,4 +44,4 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
-app.listen(port,, '0.0.0.0', () => {console.log("App is up and running on " + port)})
+app.listen(port, '0.0.0.0', () => {console.log("App is up and running on " + port)})
