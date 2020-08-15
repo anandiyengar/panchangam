0b2a486e0c1c7422fb8ad606dec62408d898cd61
anand iyengar <anandiyengar0@gmail.com>
2020-08-16 01:07:01 +0530

    FIRST COMMIT-13

--- Controller/Scraping2.js
+++ Controller/Scraping2.js
@@ -64,7 +64,12 @@ exports.getImage = (req,res) => {
   const puppeteer = require('puppeteer');
   const formId = req.form._id;
 (async () => {
-  const browser = await puppeteer.launch({ignoreDefaultArgs: ['--disable-extensions']});
+  const browser = await puppeteer.launch({
+    'args' : [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
   const desiredUrl = `https://mernpanchangam.herokuapp.com/mob/${formId}`
   const page = await browser.newPage();
   await page.setViewport({width: 540, height: 1200,
