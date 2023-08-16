9195aed87c7bdeabfd9a5d561c7f1f0b27e9aece
Anand Iyengar <anandiyengar0@gmail.com>
2023-08-16 12:04:45 +0530

    Update Scraping2.js

--- Controller/Scraping2.js
+++ Controller/Scraping2.js
@@ -1,6 +1,7 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 const { html } = require('cheerio');
+const request = require('request');
 
 const BASE_URL = 'https://calendar.goldenchennai.com/tamil-daily-calendar/';
 
@@ -16,6 +17,58 @@ exports.getDetails = async (req, res, next, dateId) => {
     /* Start processing the response */
     let $ = cheerio.load(response);
   
+    const getAccessToken = async (clientId='6615faaf-56e9-42fd-bdc0-3326ad783f22', clientSecret='hzVrQkkgFngYx6GCmpSZz75fzaMTmIp972PZ3ZHu') => {
+        const options = {
+          url: 'https://api.prokerala.com/token',
+          method: 'POST',
+          headers: {
+            'Host': 'api.prokerala.com',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+        };
+      
+        const response = await request(options);
+      
+        if (response.statusCode === 200) {
+          const data = JSON.parse(response.body);
+          return data.access_token;
+        } else {
+          throw new Error(response.statusMessage);
+        }
+      };
+
+      const getPanchang = async (accessToken, location, date) => {
+        const options = {
+          url: 'https://api.prokerala.com/v2/astrology/panchang/advanced',
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+          params: {
+            location: location,
+            date: date,
+          },
+        };
+      
+        const response = await request(options);
+      
+        if (response.statusCode === 200) {
+          const data = JSON.parse(response.body);
+          return data;
+        } else {
+          throw new Error(response.statusMessage);
+        }
+      };
+      
+      const main = async () => {
+        const accessToken = getAccessToken();
+        const location = 'Chennai, IN';
+        const dateN = '2023-08-16';
+      
+        const panchang = await getPanchang(accessToken, location, dateN);
+        console.log(panchang);
 
     const extractData = (selector, index) => {
       const data = $(selector).eq(index).text().trim();
@@ -60,28 +113,9 @@ req.details = {
       nextChandrashatama: extractData('.panchang-data-chandrashtama', 1).split(' , ')[1] || '',
    
 }
-console.log({Sunrise: header[5],
-      Sunset: header[7],
-      Ayanam: header[13],
-      day: dayData,
-      TamilMonth: TamilMonth,
-      date: date,
-      Paksham: extractData('.panchang-data-tithi', 1).split(' ')[0],
-      tithi: extractData('.panchang-data-tithi', 1).split(' ')[2],
-      tithiTime: extractData('.panchang-data-tithi', 2).split(' ')[6] + '|' + extractData('.panchang-data-tithi', 2).split(' ')[7] + ' ' + extractData('.panchang-data-tithi', 2).split(' ')[8],
-      NextTithi: extractData('.panchang-data-tithi', 3).split(' ')[2] || '',
-      nexttithiTime: extractData('.panchang-data-tithi', 4).split(' ')[6] + '|' + extractData('.panchang-data-tithi', 4).split(' ')[7] + ' ' + extractData('.panchang-data-tithi', 4).split(' ')[8] || '',
-      Nakshatram: extractData('.panchang-data-nakshatra', 1),
-      nakshatraTime: extractData('.panchang-data-nakshatra', 2).split(' ')[6] + '|' + extractData('.panchang-data-nakshatra', 2).split(' ')[7] + ' ' + extractData('.panchang-data-nakshatra', 2).split(' ')[8],
-      nextNakshatram: extractData('.panchang-data-nakshatra', 3) || '',
-      nextnakshatraTime: extractData('.panchang-data-nakshatra', 4).split(' ')[6] + '|' + extractData('.panchang-data-nakshatra', 4).split(' ')[7] + ' ' + extractData('.panchang-data-nakshatra', 4).split(' ')[8] || '',
-      rahu: extractData('.panchang-data-inauspicious-period', 2),
-      yamagandam: extractData('.panchang-data-inauspicious-period', 4),
-      kuligai: extractData('.panchang-data-inauspicious-period', 6),
-      Yogam: extractData('.panchang-data-tamil-yoga', 2),
-      Chandrashatama: extractData('.panchang-data-chandrashtama', 1).split(' , ')[0].split(' ')[1],
-      nextChandrashatama: extractData('.panchang-data-chandrashtama', 1).split(' , ')[1] || '',
-   })
+// API CALL
+
+console.log()
   }
 catch (err){
   console.log(err)
