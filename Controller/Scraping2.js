a5371721894cde11ecb6d61b936b44e028e507de
Anand Iyengar <anandiyengar0@gmail.com>
2023-08-16 11:16:52 +0530

    Update Scraping2.js

--- Controller/Scraping2.js
+++ Controller/Scraping2.js
@@ -7,6 +7,7 @@ const BASE_URL = 'https://calendar.goldenchennai.com/tamil-daily-calendar/';
 
 exports.getDetails = async (req, res, next, dateId) => {
 
+
   try{let BASE_URL = `https://www.prokerala.com/astrology/tamil-panchangam/${dateId}.html`
   
     /* Send the request to the user page and get the results */
@@ -15,37 +16,42 @@ exports.getDetails = async (req, res, next, dateId) => {
     /* Start processing the response */
     let $ = cheerio.load(response);
   
+     const extractData = (selector, index) =>
+      $(selector).text().split('\n').filter(item => item.trim() !== '' && item !== '-' && item !== '  ')[index];
+
+    const header = $('.list-wrapper').text().split('\n').filter(item => item.trim() !== '' && item !== '-' && item !== '  ');
+
     /* Parse details from the html with query selectors */
     //console.log($('.panchang-data-chandrashtama').text().split('\n').filter(item=>item.trim()!=''&&item!==" "&&item!=="-"&& item!== "  "))
   let BadTime = $('.panchang-data-inauspicious-period').text().split('\n').filter(item=>item.trim()!=''&&item!==" "&&item!=="-"&& item!== "  ")
   let TamilDay = $('.panchang-data-day').text().split('\n').filter(item=>item.trim()!=''&&item!==" "&&item!=="-"&& item!== "  ")[1]
   let tithiArray = $('.panchang-data-tithi').text().split('\n').filter(item=>item.trim()!=''&&item!==" "&&item!=="-"&& item!== "  ")
-  let header = $('.list-wrapper').text().split('\n').filter(item=>item.trim()!=''&&item!==" "&&item!=="-"&& item!== "  ")
   let yogam = $('.panchang-data-tamil-yoga').text().split('\n').filter(item=>item.trim()!=''&&item!==" "&&item!=="-"&& item!== "  ")[2]
   let chandrashtama = $('.panchang-data-chandrashtama').text().split('\n').filter(item=>item.trim()!=''&&item!==" "&&item!=="-"&& item!== "  ")[1]
   let nakshatraArray = $('.panchang-data-nakshatra').text().split('\n').filter(item=>item.trim()!=''&&item!==" "&&item!=="-"&& item!== "  ")
 req.details = {
-    Sunrise: header[5],
-    Sunset: header[7],
-    Ayanam: header[13],
-    day: TamilDay,
-    TamilMonth: TamilDay.split(",")[1]?.trim().split(" ")[0],
-    date: TamilDay.split(",")[1]?.trim().split(" ")[1],
-    Paksham: tithiArray[1].split(" ")[0],
-    tithi: tithiArray[1].split(" ")[2],
-    tithiTime: tithiArray[2].split(" ")[6]+"|"+tithiArray[2].split(" ")[7]+" "+tithiArray[2].split(" ")[8],
-    NextTithi: tithiArray[3] ?tithiArray[3].split(" ")[2]: "",
-    nexttithiTime: tithiArray[4]? tithiArray[4].split(" ")[6]+"|"+tithiArray[4].split(" ")[7]+" "+tithiArray[4].split(" ")[8]: "",
-    Nakshatram: nakshatraArray[1],
-    nakshatraTime: nakshatraArray[2].split(" ")[6]+"|"+nakshatraArray[2].split(" ")[7]+" "+nakshatraArray[2].split(" ")[8],
-    nextNakshatram: nakshatraArray[3]? nakshatraArray[3] : "",
-    nextnakshatraTime: nakshatraArray[4] ?nakshatraArray[4].split(" ")[6]+"|"+nakshatraArray[4].split(" ")[7]+" "+nakshatraArray[4].split(" ")[8]: "",
-    rahu: BadTime[2],
-    yamagandam: BadTime[4],
-    kuligai: BadTime[6],
-    Yogam: yogam,
-    Chandrashatama: chandrashtama.split(" , ")[0].split(" ")[1],
-    nextChandrashatama: chandrashtama.split(" , ")[1],
+         Sunrise: header[5],
+      Sunset: header[7],
+      Ayanam: header[13],
+      day: extractData('.panchang-data-day', 1),
+      TamilMonth: extractData('.panchang-data-day', 1).split(',')[1].trim().split(' ')[0],
+      date: extractData('.panchang-data-day', 1).split(',')[1].trim().split(' ')[1],
+      Paksham: extractData('.panchang-data-tithi', 1).split(' ')[0],
+      tithi: extractData('.panchang-data-tithi', 1).split(' ')[2],
+      tithiTime: extractData('.panchang-data-tithi', 2).split(' ')[6] + '|' + extractData('.panchang-data-tithi', 2).split(' ')[7] + ' ' + extractData('.panchang-data-tithi', 2).split(' ')[8],
+      NextTithi: extractData('.panchang-data-tithi', 3) ? extractData('.panchang-data-tithi', 3).split(' ')[2] : '',
+      nexttithiTime: extractData('.panchang-data-tithi', 4) ? extractData('.panchang-data-tithi', 4).split(' ')[6] + '|' + extractData('.panchang-data-tithi', 4).split(' ')[7] + ' ' + extractData('.panchang-data-tithi', 4).split(' ')[8] : '',
+      Nakshatram: extractData('.panchang-data-nakshatra', 1),
+      nakshatraTime: extractData('.panchang-data-nakshatra', 2).split(' ')[6] + '|' + extractData('.panchang-data-nakshatra', 2).split(' ')[7] + ' ' + extractData('.panchang-data-nakshatra', 2).split(' ')[8],
+      nextNakshatram: extractData('.panchang-data-nakshatra', 3) ? extractData('.panchang-data-nakshatra', 3) : '',
+      nextnakshatraTime: extractData('.panchang-data-nakshatra', 4) ? extractData('.panchang-data-nakshatra', 4).split(' ')[6] + '|' + extractData('.panchang-data-nakshatra', 4).split(' ')[7] + ' ' + extractData('.panchang-data-nakshatra', 4).split(' ')[8] : '',
+      rahu: extractData('.panchang-data-inauspicious-period', 2),
+      yamagandam: extractData('.panchang-data-inauspicious-period', 4),
+      kuligai: extractData('.panchang-data-inauspicious-period', 6),
+      Yogam: extractData('.panchang-data-tamil-yoga', 2),
+      Chandrashatama: extractData('.panchang-data-chandrashtama', 1).split(' , ')[0].split(' ')[1],
+      nextChandrashatama: extractData('.panchang-data-chandrashtama', 1).split(' , ')[1]
+    
 }
 
   }
